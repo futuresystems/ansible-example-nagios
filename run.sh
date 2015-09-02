14a3ee38cd26cc461924d21ff4a8e0e92deb8506
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2015-09-02 10:41:37 -0400

    test: cleanup containers on exit

--- run.sh
+++ run.sh
@@ -42,22 +42,37 @@ get_ip() {
     $docker inspect -f "{{ .NetworkSettings.IPAddress }}" $name
 }
 
+cleanup() {
+    local docker_names="$@"
+    for n in $docker_names; do
+        $docker kill $n
+        $docker rm $n
+    done
+}
+
+running_names=""
+
 # controller
-start ${name}-0 $name
+run_name=${name}-0
+start $run_name $name
 server_ip=$(get_ip ${name}-0)
-    
+docker_names="$run_name $docker_names"
 
 # clients
 client_ips=""
 for i in `seq 1 $N`; do
-    start ${name}-${i} $name
+    run_name=${name}-${i}
+    start ${run_name} $name
     client_ips="$client_ips $(get_ip ${name}-${i})"
+    docker_names="$run_name $docker_names"
 done
 
 # inventory file
 inventory_group server  ${server_ip}   > inventory.txt
 inventory_group clients ${client_ips}  >>inventory.txt
 
+# cleanup
+trap "cleanup $docker_names" SIGINT SIGTERM SIGKILL
 
 ############################################################ deploy
 
@@ -71,4 +86,6 @@ if [ -z $VIRTUAL_ENV ]; then
     . $venvdir/bin/activate
 fi
 
+# wait for containers to be running and responsive
+sleep 5s
 ansible-playbook playbook.yml
